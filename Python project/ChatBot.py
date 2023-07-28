599ff79f5429b60280653b352b3003b95273872d
Shubhkarma05 <117373278+Shubhkarma05@users.noreply.github.com>
2023-07-28 22:35:00 +0530

    Add files via upload

---
+++ Python project/ChatBot.py
@@ -0,0 +1,23 @@
+import openai
+import gradio
+
+openai.api_key = "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"
+
+messages = []
+system_msg = input("What type of chatbot would you like to create?\n")
+messages.append({"role": "system", "content": system_msg})
+
+
+def CustomChatGPT(user_input):
+    messages.append({"role": "user", "content": user_input})
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = messages
+    )
+    ChatGPT_reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": ChatGPT_reply})
+    return ChatGPT_reply
+
+demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "ChatBot")
+
+demo.launch(share=True)
